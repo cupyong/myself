5b1ce7d8964cf3a71c8e0e0353bf62d7cf0436c2
cupyong <405329943@qq.com>
2016-03-11 16:10:51 +0800

    添加pm2

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/api', function(req, res, next) {
+   res({a:1})
+});
+
 module.exports = router;
